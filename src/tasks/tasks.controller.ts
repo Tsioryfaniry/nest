ac4de017faaefcecfca213851a756cb2c8647248
Tsioryfaniry <andrianarivonyfaniry@gmail.com>
2023-10-24 14:17:06 +0300

    Learn exeption filters

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,44 @@
 
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Req, Param, HttpCode, HostParam, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Request } from 'express';
+import { CreateCatDto } from 'src/cateTodo/create-cat-dto';
 
-@Controller('tasks')
+@Controller("tasks")
 export class TasksController {
   constructor(private catsService: TasksService) {}
+    // @Get()
+    // @HttpCode(304)
+    // test(@HostParam() account: string):string{
+    //   return account
+    // }
 
-    @Get("/service")
-    getAllService(){
+    // @Get("/service")
+    // getAllService(){
 
-        return this.catsService.getAllTask()
-    }
+    //     return this.catsService.getAllTask()
+    // }
+    // @Get("/get")
+    // findAll(@Req() request:Request):string{
+    //   const userId = request.body
+    //   console.log(userId);
+      
+    //   return `User Id : ${userId}`
+    // }
+    // @Get(':id')
+    // @HttpCode(200)  
+    // findOne(@Param('id') id:string ): string{
+    //   return `This action return id : ${id}`
+    // }
+    // @Get("/async")
+    // async findAllAsync():Promise<any[]>{
+    //   return ['test',1,2]
+
+    // }
+    // @Post()
+    // async create(@Body() createCatDto:CreateCatDto){
+    //   const tasks = createCatDto
+      
+    //   return `this action creact : ${tasks}`
+    // }
 }
